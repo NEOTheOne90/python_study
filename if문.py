c92f54406fa8d9839e5c73693a9a7803f35c9485
RAMJI <changjoja0@naver.com>
2021-11-15 09:51:54 +0900

    파이썬

--- if문.py
+++ if문.py
@@ -2,5 +2,10 @@
 if True : 
     print("조건이 참이면 실행")
     print("실행코드는 띄어쓰기로 구분")
+    #if 문의 코드블록은 띄어쓰기로 된곳까지 
+elif a == b:
+    print("a b가 같다")
+else : #if문의 조건이 거짓이면 코드 실행 
+    print("if문의 조건이 거짓일때 실행") 
 
 print("종료")
---
+++ 테스트.py
@@ -0,0 +1,10 @@
+
+print('안녕하세요')
+print('이름이 무엇인가요? ')
+myName = input()
+print('만나서 반갑습니다.'+ myName)
+print('당신의 이름의 길이는: ')
+print(len(myName))
+print('당신의 나이는?')
+myAge = input()
+print('당신은 내년에 ' + str(int(myAge) + 1) + '살 입니다.') 
